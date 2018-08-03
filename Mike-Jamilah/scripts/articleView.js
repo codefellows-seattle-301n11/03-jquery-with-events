d9c22e2e959aa517cad4ceaefc66122469c22621
Jamilah McWilliams <jamilahmcw@gmail.com>
2018-08-02 19:26:54 -0700

    jamilah work

--- Mike-Jamilah/scripts/articleView.js
+++ Mike-Jamilah/scripts/articleView.js
@@ -57,11 +57,13 @@ articleView.handleAuthorFilter = function() {
 };
 
 articleView.handleCategoryFilter = function() {
+  
   // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
   // When an option with a value is selected, hide all the articles, then reveal the matches.
   // When the blank (default) option is selected, show all the articles, except for the template.
   // Be sure to reset the #author-filter while you are at it!
-
+  
+  $(`article[data-category="${category}"`).fadeIn();
 };
 
 articleView.handleMainNav = function() {
@@ -86,4 +88,5 @@ $(document).ready(function() {
 articleView.populateFilters();
 articleView.handleAuthorFilter();
 articleView.handleCategoryFilter();
+articleView.handleMainNav();
 })
