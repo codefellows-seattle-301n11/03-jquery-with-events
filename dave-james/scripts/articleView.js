ff001439143a3e507b38167e911b53236ceabf06
DAVID CERINO <DAVID.CERINO.U@GMAIL.COM>
2018-08-04 16:02:41 -0700

    about time

--- dave-james/scripts/articleView.js
+++ dave-james/scripts/articleView.js
@@ -1,30 +1,30 @@
 'use strict';
 
-// REVIEW: Configure an object to hold all of our functions for dynamic updates and article-related event handlers.
+// REVIEWED: Configure an object to hold all of our functions for dynamic updates and article-related event handlers.
 let articleView = {};
 
 articleView.populateFilters = function() {
   $('article').each(function() {
-    // REVIEW: We can declare several variables at once and assign their values later when using let. Keep in mind that we cannot do this with const.
+    // REVIEWED: We can declare several variables at once and assign their values later when using let. Keep in mind that we cannot do this with const.
     let authorName, category, optionTag;
     if (!$(this).hasClass('template')) {
-      // REVIEW: We need to take every author name from the page, and make it an option in the Author filter.
+      // REVIEWED: We need to take every author name from the page, and make it an option in the Author filter.
       // To do so, Build an <option> DOM element that we can append to the author <select> element.
       // Start by grabbing the author's name from `this` article element, and then use that bit of text to create the option tag (in a variable named `optionTag`) that we can append to the #author-filter select element.
       authorName = $(this).attr('data-author');
 
       // COMPLETE: Refactor this concatenation using a template literal.
-      optionTag = `"<option value="${authorName}>" ${authorName}"</option>"`;
+      optionTag = `<option value="${authorName}">${authorName}</option>`;
 
       if ($(`#author-filter option[value="${authorName}"]`).length === 0) {
         $('#author-filter').append(optionTag);
       }
 
-      // REVIEW: Similar to the above, but...
+      // REVIEWED: Similar to the above, but...
       // Avoid duplicates! We don't want to append the category name if the <select> already has this category as an option!
       category = $(this).attr('data-category');
-
-      // TODO: Refactor this concatenation using a template literal.
+    
+      // COMPLETE: Refactor this concatenation using a template literal.
       optionTag = `<option value="${category}">${category}</option>`;
 
       if ($(`#category-filter option[value="${category}"]`).length === 0) {
@@ -76,5 +76,6 @@ articleView.setTeasers = function() {
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
+  // call the functions
 
 })
