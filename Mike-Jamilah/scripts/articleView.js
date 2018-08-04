66041927cc96446e2cc059c6caf98d434405e133
Mike K <m.kermes89@gmail.com>
2018-08-04 12:02:02 -0700

    found missing ]

--- Mike-Jamilah/scripts/articleView.js
+++ Mike-Jamilah/scripts/articleView.js
@@ -15,7 +15,7 @@ articleView.populateFilters = function() {
 
       // TODONE: Refactor this concatenation using a template literal.
       optionTag = `<option value="${authorName}">${authorName}</option>`;
-      
+
       if ($('#author-filter option[value="' + authorName + '"]').length === 0) {
         $('#author-filter').append(optionTag);
       }
@@ -45,34 +45,31 @@ articleView.handleAuthorFilter = function() {
       $('article').show();
 
       $('.template').hide();
- 
-     }
-     $('#category-filter').val('');
-   });
- };
-      // REVIEWED: Inside this function, "this" is the element that triggered the event handler function we are defining. "$(this)" is using jQuery to select that element (analogous to event.target that we have seen before), so we can chain jQuery methods onto it.
-    
-      // TODONE: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
-      // Use an "attribute selector" to find those articles, and fade them in for the reader.
-      
-      // TODONE: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
 
-     
+    }
+    $('#category-filter').val('');
+  });
+};
+// REVIEWED: Inside this function, "this" is the element that triggered the event handler function we are defining. "$(this)" is using jQuery to select that element (analogous to event.target that we have seen before), so we can chain jQuery methods onto it.
+
+// TODONE: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
+// Use an "attribute selector" to find those articles, and fade them in for the reader.
+
+// TODONE: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
+
 
 articleView.handleCategoryFilter = function() {
   $('#category-filter').on('change', function() {
     if ($(this).val()){
       $('article').hide();
-      
+      // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
+      // When an option with a value is selected, hide all the articles, then reveal the matches.
+      // When the blank (default) option is selected, show all the articles, except for the template.
+      // Be sure to reset the #author-filter while you are at it!
 
-  // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
-  // When an option with a value is selected, hide all the articles, then reveal the matches.
-  // When the blank (default) option is selected, show all the articles, except for the template.
-  // Be sure to reset the #author-filter while you are at it!
-  
-  $(`article[data-category="${category}"`).fadeIn();
+      $(`article[data-category="${category}"]`).fadeIn();
     } else {
-      $('article').show();      
+      $('article').show();
       $('.template').hide();
 }});
 
@@ -93,10 +90,10 @@ articleView.setTeasers = function() {
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
 };
 
-// TODONE-ISH: Call all of the above functions, once we are sure the DOM is ready.
-$(document).ready(function() {
-articleView.populateFilters();
-articleView.handleAuthorFilter();
-articleView.handleCategoryFilter();
-articleView.handleMainNav();
+  // TODONE-ISH: Call all of the above functions, once we are sure the DOM is ready.
+  $(document).ready(function() {
+    articleView.populateFilters();
+    articleView.handleAuthorFilter();
+    articleView.handleCategoryFilter();
+    articleView.handleMainNav();
 })};
